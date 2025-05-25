aae330da5ab6ae76a9f051a98f6038b7ef658b96
OfficalMilkUp <lypos8489@gmail.com>
2025-05-25 11:55:02 +0900

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -2682,7 +2682,7 @@ end;
 do
     Library.NotificationArea = Library:Create('Frame', {
         BackgroundTransparency = 1;
-        Position = UDim2.new(0, 0, 0, 40);
+        Position = UDim2.new(0, 809, 0, -25);
         Size = UDim2.new(0, 300, 0, 200);
         ZIndex = 100;
         Parent = ScreenGui;
