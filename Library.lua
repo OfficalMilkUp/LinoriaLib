b10daf39f742eda1889fc01ee1126791e9f0fc52
OfficalMilkUp <lypos8489@gmail.com>
2025-05-25 12:07:11 +0900

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -2682,7 +2682,7 @@ end;
 do
     Library.NotificationArea = Library:Create('Frame', {
         BackgroundTransparency = 1;
-        Position = UDim2.new(0, 814, 0, -5);
+        Position = UDim2.new(0, 100, 0, -5);
         Size = UDim2.new(0, 300, 0, 200);
         ZIndex = 100;
         Parent = ScreenGui;
@@ -2697,7 +2697,7 @@ do
 
     local WatermarkOuter = Library:Create('Frame', {
         BorderColor3 = Color3.new(0, 0, 0);
-        Position = UDim2.new(0, 814, 0, -5);
+        Position = UDim2.new(0, 100, 0, -5);
         Size = UDim2.new(0, 213, 0, 20);
         ZIndex = 200;
         Visible = false;
